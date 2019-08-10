c8b319db9b53197178e78f368f004dd2b02b3c33
Shiraaz Ahammed <shiraaz.ahammed@gmail.com>
2019-08-11 01:02:02 +0530

    Async testing: To check if feed has atleast one entry

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -79,15 +79,25 @@ $(function() {
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
-
-        /* TODO: Write a test that ensures when the loadFeed
+    describe('Inital Entries', function() {
+                /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        beforeEach(function(done) {
+            loadFeed(0, done);
+        })
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+        it('Async has atleast one entry', function(done) {
+            expect(document.querySelector('.feed').getElementsByClassName('entry').length).not.toBe(0);
+            done();
+        })
+
+    })
+
+        /* TODO: Write a new test suite named "New Feed Selection" */
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
